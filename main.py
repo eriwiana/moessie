c528e59fd24e9fe568b2d23c43afe1fb04de6f74
Eri Wiana <eriwiana@gmail.com>
2023-01-14 23:47:05 +0700

    Updated main.py to use settings

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from base import settings
 from routes.subscription import router as subscription_router
 
-app = FastAPI(title="Moessie", docs_url=None, openapi_url=None, redoc_url=None)
-app.mount("/static", StaticFiles(directory="dashboard/static"), name="static")
+app = FastAPI(
+    title=settings.app_name, docs_url=None, openapi_url=None, redoc_url=None
+)
+
+# Serve Static files
+app.mount(
+    settings.static_path,
+    StaticFiles(directory=settings.static_dir),
+    name=settings.static_name,
+)
 
 app.include_router(subscription_router)
